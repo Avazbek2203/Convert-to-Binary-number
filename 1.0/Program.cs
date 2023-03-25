e39043232351a00eac3c29e33ee59ee8aa629c64
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-25 14:36:34 +0500

    Add project files.

---
+++ 1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a decimal number: ");
+        int decimalNumber;
+        while (!int.TryParse(Console.ReadLine(), out decimalNumber))
+        {
+            Console.WriteLine("Enter only an integer number, please! ");
+            Console.Write("Enter a decimal number: ");
+        }
+
+        string binaryNumber = Convert.ToString(decimalNumber, 2);
+        Console.WriteLine("Binary number: " + binaryNumber);
+    }
+}
